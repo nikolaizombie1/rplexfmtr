737c7febf748ec97aea0e74dc6ad8ea4da09d7f2
nikolaizombie1 <fabio.matos999@gmail.com>
2023-06-19 20:14:55 -0400

    Created verify name method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod database;
 use clap::Parser;
-use std::{eprintln, path::PathBuf, println,  process::exit};
+use std::{
+    eprintln,
+    fs::read_dir,
+    io,
+    path:: PathBuf,
+    println,
+    process::exit,
+};
+use regex::RegexSet;
+use lazy_static::{LazyStatic, lazy_static};
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -8,14 +17,34 @@ struct Cli {
     /// Path of video folder(s)
     #[arg(short, long, value_parser = valid_paths, num_args = 1.. )]
     path: Vec<PathBuf>,
+
+    /// Output Folder for Plex formatted media
+    #[arg(short,long,value_parser = valid_paths, num_args = 1) ]
+    output_path: PathBuf,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let db = database::setup_database(database::URL).await?;
     let args = Cli::parse();
-    println!("You've entered {} directories.", args.path.len());
-
+    for path in &args.path {
+        let mut name = std::ffi::OsStr::new("");
+        loop {
+            println!(
+                "What would you like the entries for {} to be tittled?: ",
+                path.to_str().unwrap()
+            );
+            let mut ans = String::new();
+            io::stdin().read_line(&mut ans)?;
+            if !valid_name(&ans) {
+                continue;
+            }
+            else {
+                name = std::ffi::OsStr::new(&ans);
+                break;
+            }
+        }
+    }
     Ok(())
 }
 
@@ -26,4 +55,23 @@ fn valid_paths(s: &str) -> anyhow::Result<PathBuf> {
         exit(1);
     }
     Ok(path)
+}
+
+fn print_directory(path: PathBuf) -> anyhow::Result<()> {
+    for file in read_dir(path)? {
+        println!("{}", file?.file_name().to_str().unwrap());
+    }
+    Ok(())
+}
+
+fn valid_name(name: &str) -> bool {
+    lazy_static! {
+        static ref REGEXES: RegexSet = RegexSet::new(&[r#"([<>:"/\|?*\\])"#, r#"COM[0-9]"#, r#"NUL"#, r#"PRN"#, r#"AUX"# ]).unwrap();
+    }
+    if name.len() == 0 || REGEXES.is_match(name) || name.contains("\0") || name.chars().nth(name.len() - 2).unwrap() == '.' || name.chars().nth(name.len() - 2).unwrap() == ' ' {
+        false
+    }
+    else {
+        true
+    }
 }
